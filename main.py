2b139f5d2857e2940f649f8f9c56fcecff5df20a
Anthony Eid <anthonyeid7@outlook.com>
2022-10-09 22:04:00 -0400

    Add edit message alert.

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from tokens import TOKEN
 def main():
 
     intents = discord.Intents.default()
+    
+    # Enable needed intents
     intents.message_content = True
+    intents.messages = True
 
     client = discord.Client(intents=intents)
 
@@ -20,6 +23,17 @@ def main():
 
         if message.content.startswith('$hello'):
             await message.channel.send('Hello!')
+            
+    @client.event
+    async def on_message_edit(before: discord.Message, after: discord.Message):
+        """Output the edited message of a user
+
+        Args:
+            before (str): The message before it was edited 
+            after (str): The message after it was edited
+        """
+        await before.channel.send(f'User {after.author} edited this message from \"{before.content}\"')
+        
 
     client.run(TOKEN)
 
